643e1d0b64541276ab012859eeaaa87a0c8336fb
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2018-07-13 01:05:29 +0200

    os/windows/waitable: Fix wait/wait_any/wait_all and add callback calling
    Before this fix, wait/wait_any/wait_all would attempt to wait the same time again instead of reducing the timeout time by the number of milliseconds passed, resulting in a drastically longer wait than expected if the thread was running any kind of APC code.

--- source/os/windows/waitable.cpp
+++ source/os/windows/waitable.cpp
@@ -15,35 +15,40 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "../waitable.hpp"
-
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+
 #include <windows.h>
+#include "../async_op.hpp"
+#include "../waitable.hpp"
 
 os::error os::waitable::wait(waitable *item, std::chrono::nanoseconds timeout) {
-	int64_t ms_timeout = std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count();
 	HANDLE  handle     = (HANDLE)item->get_waitable();
+	int64_t ms_timeout = std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count();
 
-	SetLastError(ERROR_SUCCESS);
+wait_retry:
+	auto start = std::chrono::high_resolution_clock::now();
+	if (ms_timeout < 0) {
+		ms_timeout = 0;
+	}
+
 	DWORD result = WaitForSingleObjectEx(handle, DWORD(ms_timeout), TRUE);
 	if (result == WAIT_OBJECT_0) {
-		return os::error::Success;
-	} else if (result = WAIT_TIMEOUT) {
-		return os::error::TimedOut;
-	} else if (result = WAIT_ABANDONED) {
-		return os::error::Disconnected; // Disconnected Semaphore from original Owner
-	} else if (result = WAIT_IO_COMPLETION) {
-		SetLastError(ERROR_SUCCESS);
-		result = WaitForSingleObjectEx(handle, DWORD(ms_timeout), TRUE);
-		if (result == WAIT_OBJECT_0) {
-			return os::error::Success;
-		} else if (result = WAIT_TIMEOUT) {
-			return os::error::TimedOut;
-		} else if (result = WAIT_ABANDONED) {
-			return os::error::Disconnected; // Disconnected Semaphore from original Owner
+		os::async_op *aop = dynamic_cast<os::async_op *>(item);
+		if (aop) {
+			aop->call_callback();
 		}
+		return os::error::Success;
+	} else if (result == WAIT_TIMEOUT) {
+		return os::error::TimedOut;
+	} else if (result == WAIT_ABANDONED) {
+		return os::error::Disconnected; // Disconnected Semaphore from original Owner
+	} else if (result == WAIT_IO_COMPLETION) {
+		ms_timeout -=
+			std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start)
+				.count();
+		goto wait_retry;
 	}
 	return os::error::Error;
 }
@@ -63,26 +68,45 @@ os::error os::waitable::wait_any(waitable **items, size_t items_count, size_t &s
 	// Need to create a sequential array of HANDLEs here.
 	size_t              valid_handles = 0;
 	std::vector<HANDLE> handles(items_count);
+	std::vector<size_t> idxToTrueIdx(items_count);
 	for (size_t idx = 0, eidx = items_count; idx < eidx; idx++) {
 		waitable *obj = items[idx];
 		if (obj) {
 			handles[valid_handles] = (HANDLE)obj->get_waitable();
+			idxToTrueIdx[idx]      = valid_handles;
 			valid_handles++;
-		}		
+		}
 	}
 
 	int64_t ms_timeout = std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count();
 
-	DWORD result = WaitForMultipleObjects(DWORD(valid_handles), handles.data(), false, DWORD(ms_timeout));
+wait_any_retry:
+	auto start = std::chrono::high_resolution_clock::now();
+	if (ms_timeout < 0) {
+		ms_timeout = 0;
+	}
+
+	DWORD result = WaitForMultipleObjectsEx(DWORD(valid_handles), handles.data(), FALSE, DWORD(ms_timeout), TRUE);
 	if ((result >= WAIT_OBJECT_0) && result < (WAIT_OBJECT_0 + MAXIMUM_WAIT_OBJECTS)) {
-		signalled_index = result - WAIT_OBJECT_0;
+		signalled_index = idxToTrueIdx[result - WAIT_OBJECT_0];
+
+		os::async_op *aop = dynamic_cast<os::async_op *>(items[signalled_index]);
+		if (aop) {
+			aop->call_callback();
+		}
+
 		return os::error::Success;
-	} else if (result = WAIT_TIMEOUT) {
+	} else if (result == WAIT_TIMEOUT) {
 		signalled_index = -1;
 		return os::error::TimedOut;
 	} else if ((result >= WAIT_ABANDONED_0) && result < (WAIT_ABANDONED_0 + MAXIMUM_WAIT_OBJECTS)) {
-		signalled_index = result - WAIT_ABANDONED_0;
+		signalled_index = idxToTrueIdx[result - WAIT_ABANDONED_0];
 		return os::error::Disconnected; // Disconnected Semaphore from original Owner
+	} else if (result == WAIT_IO_COMPLETION) {
+		ms_timeout -=
+			std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start)
+				.count();
+		goto wait_any_retry;
 	}
 	return os::error::Error;
 }
@@ -111,26 +135,47 @@ os::error os::waitable::wait_all(waitable **items, size_t items_count, size_t &s
 	// Need to create a sequential array of HANDLEs here.
 	size_t              valid_handles = 0;
 	std::vector<HANDLE> handles(items_count);
+	std::vector<size_t> idxToTrueIdx(items_count);
 	for (size_t idx = 0, eidx = items_count; idx < eidx; idx++) {
 		waitable *obj = items[idx];
 		if (obj) {
 			handles[valid_handles] = (HANDLE)obj->get_waitable();
+			idxToTrueIdx[idx]      = valid_handles;
 			valid_handles++;
 		}
 	}
 
 	int64_t ms_timeout = std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count();
 
-	DWORD result = WaitForMultipleObjects(DWORD(valid_handles), handles.data(), true, DWORD(ms_timeout));
+wait_all_retry:
+	auto start = std::chrono::high_resolution_clock::now();
+	if (ms_timeout < 0) {
+		ms_timeout = 0;
+	}
+
+	DWORD result = WaitForMultipleObjectsEx(DWORD(valid_handles), handles.data(), true, DWORD(ms_timeout), TRUE);
 	if ((result >= WAIT_OBJECT_0) && result < (WAIT_OBJECT_0 + MAXIMUM_WAIT_OBJECTS)) {
 		signalled_index = result - WAIT_OBJECT_0;
+
+		for (size_t idx = 0; idx < items_count; idx++) {
+			os::async_op *aop = dynamic_cast<os::async_op *>(items[idx]);
+			if (aop) {
+				aop->call_callback();
+			}
+		}
+
 		return os::error::Success;
-	} else if (result = WAIT_TIMEOUT) {
+	} else if (result == WAIT_TIMEOUT) {
 		signalled_index = -1;
 		return os::error::TimedOut;
 	} else if ((result >= WAIT_ABANDONED_0) && result < (WAIT_ABANDONED_0 + MAXIMUM_WAIT_OBJECTS)) {
 		signalled_index = result - WAIT_ABANDONED_0;
 		return os::error::Disconnected; // Disconnected Semaphore from original Owner
+	} else if (result == WAIT_IO_COMPLETION) {
+		ms_timeout -=
+			std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start)
+				.count();
+		goto wait_all_retry;
 	}
 	return os::error::Error;
 }
